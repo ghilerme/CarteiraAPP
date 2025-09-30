0d850bac4539f1ba1bcb27237110175cd568bf19
ths <thalitasanttos77@gmail.com>
2025-09-30 20:38:48 -0300

    feat/implementa dashboard

--- app/src/main/java/com/example/carteira/controller/MainActivity.kt
+++ app/src/main/java/com/example/carteira/controller/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.carteira.controller
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.carteira.R
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +19,23 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val btnCadastro = findViewById<Button>(R.id.btn_cadastro)
+        val btnExtrato = findViewById<Button>(R.id.btn_extrato)
+        val btnSair = findViewById<Button>(R.id.btn_sair)
+
+        btnCadastro.setOnClickListener {
+            val intent = Intent(this, Operacoes::class.java)
+            startActivity(intent)
+        }
+
+        btnExtrato.setOnClickListener {
+            val intent = Intent(this, Extrato::class.java)
+            startActivity(intent)
+        }
+
+        btnSair.setOnClickListener {
+            finish()
         }
     }
 }
