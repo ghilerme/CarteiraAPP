bb03bdd8d715041479845979cf6c585decd5e46e
vkataguiri <viniciuskataguiri@gmail.com>
2025-09-30 18:38:44 -0300

    chore: reestructuring packages

--- app/src/main/java/com/example/carteira/controller/Extrato.kt
+++ app/src/main/java/com/example/carteira/controller/Extrato.kt
@@ -1,16 +1,17 @@
-package com.example.carteira
+package com.example.carteira.controller
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.carteira.R
 
-class Operacoes : AppCompatActivity() {
+class Extrato : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_operacoes)
+        setContentView(R.layout.activity_extrato)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
